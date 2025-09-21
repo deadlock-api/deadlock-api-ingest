5bdaa372fc3b9174e64405dc9236e81eb977a80c
molecule31 <73964434+molecule31@users.noreply.github.com>
2025-09-21 18:04:47 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A network packet capture tool that monitors HTTP traffic for Deadlock game replay files and ingests metadata to the Deadlock API.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # Adjust if needed
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,9 +12,15 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
         package = import ./default.nix { inherit pkgs; };
       in {
+        apps.default = {
+          type = "app";
+          program = "${package}/bin/deadlock-api-ingest";
+        };
+
         packages.default = package;
+
       });
 }
-
