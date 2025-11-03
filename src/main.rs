e8519e3d0a0c36b8e9df5ef8ad85363077c7d249
Manuel Raimann <raimannma@outlook.de>
2025-11-03 21:58:55 +0100

    refactor: remove http listener

--- src/main.rs
+++ src/main.rs
@@ -10,56 +10,30 @@
 #![deny(clippy::std_instead_of_core)]
 #![allow(clippy::unreadable_literal)]
 
-use crate::http_listener::{HttpListener, PlatformListener};
-
 mod error;
-mod http_listener;
 mod ingestion_cache;
 mod scan_cache;
 mod utils;
 
 fn main() {
-    // Check for --once flag
     let once_mode = std::env::args().any(|arg| arg == "--once");
-
-    if once_mode {
-        println!("Running in once mode: performing initial cache ingest only");
-        let cache_dir = scan_cache::get_cache_directory();
-        if let Some(cache_dir) = cache_dir {
-            scan_cache::initial_cache_dir_ingest(&cache_dir);
-            println!("Initial cache ingest completed. Exiting.");
-        } else {
-            eprintln!("Could not find cache directory. Exiting.");
-        }
-        return;
-    }
-
-    let mut handles = Vec::new();
-
-    handles.push(std::thread::spawn(|| {
-        loop {
-            if let Err(e) = PlatformListener.listen() {
-                eprintln!("Error in HTTP listener: {e:?}");
-            }
-            std::thread::sleep(core::time::Duration::from_secs(10));
-        }
-    }));
 
     let cache_dir = scan_cache::get_cache_directory();
     if let Some(cache_dir) = cache_dir {
-        handles.push(std::thread::spawn(move || {
+        if once_mode {
+            println!("Running in once mode: performing initial cache ingest only");
             scan_cache::initial_cache_dir_ingest(&cache_dir);
-            loop {
-                if let Err(e) = scan_cache::watch_cache_dir(&cache_dir) {
-                    eprintln!("Error in cache watcher: {e:?}");
-                }
-                std::thread::sleep(core::time::Duration::from_secs(10));
-            }
-        }));
-    }
+            println!("Initial cache ingest completed. Exiting.");
+        }
 
-    #[allow(clippy::never_loop)]
-    for handle in handles {
-        handle.join().unwrap();
+        scan_cache::initial_cache_dir_ingest(&cache_dir);
+        loop {
+            if let Err(e) = scan_cache::watch_cache_dir(&cache_dir) {
+                eprintln!("Error in cache watcher: {e:?}");
+            }
+            std::thread::sleep(core::time::Duration::from_secs(10));
+        }
+    } else {
+        eprintln!("Could not find cache directory. Exiting.");
     }
 }
